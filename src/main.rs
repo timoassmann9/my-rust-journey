ab9265e2bce4a865e85ca5289de1da56bfdc20bf
timoassmann9 <timo.assmann@web.de>
2026-03-25 19:21:48 +0100

    getting started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-}
+// Hello World
+// fn main() {
+//     println!("Hello, world!");
+//     println!("I'm a Rustacean!");
+// }
+
+// Format
+// fn main() {
+//     let pi = 3.141592;
+//     println!("Pi is roughly {pi:.5}");
+// }
