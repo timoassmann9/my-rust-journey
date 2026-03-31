4bec99ded0516dc2aebf0c7623e2419381b94f67
timoassmann9 <timo.assmann@web.de>
2026-03-31 20:25:50 +0200

    add exercise 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    // exercise 1
+    let name = "Timo";
+    let mut count: u32 = 0;
+
+    for _ in 1..=5 {
+        count += 1;
+        println!("Hello, {}! (count: {})", name, count);
+    }
+
+    let result = match count % 2 {
+        0 => "even",
+        _ => "odd",
+    };
+    println!("\nCount is {} after {} counts", result, count);
+
+    // exercise 2
+    
 }
