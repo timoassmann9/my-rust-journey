7ccda3c8c7c0d4ce0802841ffd9b00928287c147
timoassmann9 <timo.assmann@web.de>
2026-03-31 22:11:54 +0200

    add exercise 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,19 +1,33 @@
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    c * 9.0 / 5.0 + 32.0
+}
+
+fn classify(temp_f: f64) -> &'static str {
+    if temp_f > 75.0 { "hot" } 
+    else if temp_f > 60.0 { "mild" } 
+    else { "cold" }
+}
+
 fn main() {
     // exercise 1
-    let name = "Timo";
-    let mut count: u32 = 0;
+    // let name = "Timo";
+    // let mut count: u32 = 0;
 
-    for _ in 1..=5 {
-        count += 1;
-        println!("Hello, {}! (count: {})", name, count);
-    }
+    // for _ in 1..=5 {
+    //     count += 1;
+    //     println!("Hello, {}! (count: {})", name, count);
+    // }
 
-    let result = match count % 2 {
-        0 => "even",
-        _ => "odd",
-    };
-    println!("\nCount is {} after {} counts", result, count);
+    // let result = match count % 2 {
+    //     0 => "even",
+    //     _ => "odd",
+    // };
+    // println!("\nCount is {} after {} counts", result, count);
 
     // exercise 2
-    
+    let values = vec![0.0, 20.0, 35.0];
+    for value in values {
+        let fahrenheit = celsius_to_fahrenheit(value);
+        println!("{value:<2}° Celsius ({fahrenheit:.2}° Fahrenheit) feels {}", classify(fahrenheit));
+    }
 }
