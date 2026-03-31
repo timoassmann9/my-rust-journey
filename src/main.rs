790d794bb7586f50929a70ac33efc6a785af0f66
timoassmann9 <timo.assmann@web.de>
2026-03-31 16:39:44 +0200

    add exercise no.1

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,18 @@
 //     let pi = 3.141592;
 //     println!("Pi is roughly {pi:.5}");
 // }
+
+fn main() {
+    let name = "Timo";
+    let mut count = 0;
+
+    for _ in 1..=5 {
+        count += 1;
+        println!("Hello, {}! (count: {})", name, count);
+    }
+    let parity = match count % 2 {
+        0 => "even",
+        _ => "odd",
+    };
+    println!("Final count {} is {}", count, parity);
+}
