d41cd2ef9ed437a90bd2387977b392ae7f8f50cb
timoassmann9 <timo.assmann@web.de>
2026-04-22 15:44:37 +0200

    complete exercise 04-02

--- lessons/lesson-04-error-handling-modules-crates/src/main.rs
+++ lessons/lesson-04-error-handling-modules-crates/src/main.rs
@@ -1,46 +1,10 @@
-#[derive(Debug)]
-enum ConfigError {
-    MissingValue,
-    InvalidNumber,
-    OutOfRange,
-}
-
-fn parse_port(raw: &str) -> Result<u16, ConfigError> {
-    if raw.trim().is_empty() { return Err(ConfigError::MissingValue); }
-    match raw.trim().parse::<u32>() {
-        Ok(x) =>  {
-            match u16::try_from(x) {
-                Ok(x) => Ok(x),
-                Err(_) => Err(ConfigError::OutOfRange),
-            }
-        },
-        Err(_) => Err(ConfigError::InvalidNumber)
-    }
-
-    /*
-    Alternative:
-
-    let trimmed = raw.trim();
-    if trimmed.is_empty() {
-        return Err(ConfigError::MissingValue);
-    }
-
-    let parsed = trimmed
-        .parse::<u32>()
-        .map_err(|_| ConfigError::InvalidNumber)?;
-
-    if parsed > u16::MAX as u32 {
-        return Err(ConfigError::OutOfRange);
-    }
-
-    Ok(parsed as u16)
-     */
-}
+mod config;
+mod error;
 
 fn main() {
     let vals = vec!["2", "", "300", "-16 ", "70000", "abc"];
     for val in vals {
-        let result = parse_port(val);
+        let result = config::parse_port(val);
         println!("{result:?}");
     }
 }
