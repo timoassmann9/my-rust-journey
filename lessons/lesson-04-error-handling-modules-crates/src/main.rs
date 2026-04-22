bd5d1431167427bae8822395bc9c55f41a7aac18
timoassmann9 <timo.assmann@web.de>
2026-04-22 15:06:05 +0200

    complete exercise 04-01

--- lessons/lesson-04-error-handling-modules-crates/src/main.rs
+++ lessons/lesson-04-error-handling-modules-crates/src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+enum ConfigError {
+    MissingValue,
+    InvalidNumber,
+    OutOfRange,
+}
+
+fn parse_port(raw: &str) -> Result<u16, ConfigError> {
+    if raw.is_empty() { return Err(ConfigError::MissingValue); }
+    match raw.trim().parse::<u32>() {
+        Ok(x) =>  {
+            if u16::MIN as u32 <= x && x <= u16::MAX as u32 { Ok(x as u16) }
+            else { Err(ConfigError::OutOfRange) }
+        },
+        Err(_) => { Err(ConfigError::InvalidNumber) }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let vals = vec!["2", "", "300", "-16 ", "70000", "abc"];
+    for val in vals {
+        let result = parse_port(val);
+        println!("{result:?}");
+    }
 }
